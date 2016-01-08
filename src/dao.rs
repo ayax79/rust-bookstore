7a77ac4e2ad41ebf4e33ad197d533cc0fcc6afb7
Jack Wright <jack.wright@nike.com>
2016-01-07 16:07:45 -0800

    more cleanup

--- src/dao.rs
+++ src/dao.rs
@@ -14,56 +14,55 @@ impl <'a> BookDao<'a> {
     }
 
     pub fn put(&mut self, entry: &Book) -> Result<(), DynamoDBError> {
-        let item = build_put_item_input(entry);
+        let item = BookDao::build_put_item_input(entry);
         try!(self.dynamodb.as_mut().put_item(&item));
         Ok(())
     }
 
     pub fn get(&mut self, uuid: &Uuid) -> Result<Option<Book>, DynamoDBError> {
-        let request = create_get_item_input(uuid);
+        let request = BookDao::create_get_item_input(uuid);
 
         match self.dynamodb.as_mut().get_item(&request) {
             Ok(item) => {
-                Ok(item.Item.map(|item_map| read_entry(item_map)))
+                Ok(item.Item.map(|item_map| BookDao::read_entry(item_map)))
             }
             Err(err) => Err(err)
         }
     }
 
-}
-
-fn read_entry(item_map: HashMap<String, AttributeValue>) -> Book {
-    Book {
-        book_id: get_uuid_from_attribute(&item_map.get("book_id").unwrap()).unwrap(),
-        author: get_str_from_attribute(&item_map.get("author").unwrap()).unwrap().to_string(),
-        title: get_str_from_attribute(&item_map.get("title").unwrap()).unwrap().to_string()
+    fn read_entry(item_map: HashMap<String, AttributeValue>) -> Book {
+        Book {
+            book_id: get_uuid_from_attribute(&item_map.get("book_id").unwrap()).unwrap(),
+            author: get_str_from_attribute(&item_map.get("author").unwrap()).unwrap().to_string(),
+            title: get_str_from_attribute(&item_map.get("title").unwrap()).unwrap().to_string()
+        }
     }
-}
 
-fn build_put_item_input(entry: &Book) -> PutItemInput {
-    let mut input = PutItemInput::default();
-    input.Item = create_put_item_map(entry);
-    input.TableName = BOOKS_TABLE.to_string();
-    return input;
-}
+    fn build_put_item_input(entry: &Book) -> PutItemInput {
+        let mut input = PutItemInput::default();
+        input.Item = BookDao::create_put_item_map(entry);
+        input.TableName = BOOKS_TABLE.to_string();
+        return input;
+    }
 
-fn create_put_item_map(entry: &Book) -> PutItemInputAttributeMap {
-    let mut item_map = PutItemInputAttributeMap::default();
-    item_map.insert("book_id".to_string(), val!(S => entry.book_id.to_urn_string()));
-    item_map.insert("author".to_string(), val!(S => entry.author));
-    item_map.insert("title".to_string(), val!(S => entry.title));
-    return item_map;
-}
+    fn create_put_item_map(entry: &Book) -> PutItemInputAttributeMap {
+        let mut item_map = PutItemInputAttributeMap::default();
+        item_map.insert("book_id".to_string(), val!(S => entry.book_id.to_urn_string()));
+        item_map.insert("author".to_string(), val!(S => entry.author));
+        item_map.insert("title".to_string(), val!(S => entry.title));
+        return item_map;
+    }
 
-fn create_key(uuid: &Uuid) -> Key {
-    let mut key = Key::default();
-    key.insert("book_id".to_string(), val!(S => uuid.to_urn_string()));
-    return key;
-}
+    fn create_key(uuid: &Uuid) -> Key {
+        let mut key = Key::default();
+        key.insert("book_id".to_string(), val!(S => uuid.to_urn_string()));
+        return key;
+    }
 
-fn create_get_item_input(uuid: &Uuid) -> GetItemInput {
-    let mut request = GetItemInput::default();
-    request.Key = create_key(uuid);
-    request.TableName = BOOKS_TABLE.to_string();
-    return request;
+    fn create_get_item_input(uuid: &Uuid) -> GetItemInput {
+        let mut request = GetItemInput::default();
+        request.Key = BookDao::create_key(uuid);
+        request.TableName = BOOKS_TABLE.to_string();
+        return request;
+    }
 }
