6ea3834b1c9a7e3e88b28467169cbf1ad8e025e2
Jack Wright <jack.wright@nike.com>
2016-02-11 14:21:51 -0800

    basic iron setup

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,57 @@
 #[macro_use]
 extern crate rusoto;
 extern crate uuid;
+extern crate iron;
+extern crate router;
+extern crate rustc_serialize;
 
-mod dao;
-mod model;
 mod dynamo_utils;
+mod model;
+mod dao;
 
-use dao::BookDao;
-use rusoto::dynamodb::DynamoDBError;
+use std::io::Read;
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+use rustc_serialize::json;
 use model::Book;
 use uuid::Uuid;
+use dao::BookDao;
+use rusoto::dynamodb::DynamoDBError;
+
+// mod dao;
+// mod model;
+//
+// use dao::BookDao;
+// use rusoto::dynamodb::DynamoDBError;
 
 fn main() {
-    let mut dao = BookDao::new();
+    let mut router = Router::new();
+    router.get("/books", book_list);
+    router.put("/books", create_book);
 
-    let book0 = Book {
-        book_id: Uuid::new_v4(),
-        author: "Ernest Hemmingway".to_string(),
-        title: "For Whom the Bell Tolls".to_string()
-    };
-    print_put(&mut dao, &book0);
 
-    let book_result0: Result<Option<Book>, DynamoDBError> = dao.get(&book0.book_id);
-    print_result(&book0.book_id, book_result0);
+    fn book_list(_: &mut Request) -> IronResult<Response> {
+        let book0 = Book {
+            book_id: Uuid::new_v4(),
+            author: "Ernest Hemmingway".to_string(),
+            title: "For Whom the Bell Tolls".to_string()
+        };
+        let payload = json::encode(&book0).unwrap();
+        Ok(Response::with((status::Ok, payload)))
+    }
+
+    fn create_book(request: &mut Request) -> IronResult<Response> {
+        let mut dao = BookDao::new();
+        let mut payload = String::new();
+        request.body.read_to_string(&mut payload).unwrap();
+        let book: Book = json::decode(&payload).unwrap();
+        print_put(&mut dao, &book);
+        Ok(Response::with((status::Ok, payload)))
+    }
+
+    println!("Starting bookstore server on port 3000");
+    Iron::new(router).http("localhost:3000").unwrap();
 }
 
 fn print_put(dao: &mut BookDao, book: &Book) -> () {
@@ -53,3 +82,18 @@ fn print_result(book_id: &Uuid, result: Result<Option<Book>, DynamoDBError>) -> 
         }
     }
 }
+
+
+// fn main() {
+//     let mut dao = BookDao::new();
+//
+//     let book0 = Book {
+//         book_id: Uuid::new_v4(),
+//         author: "Ernest Hemmingway".to_string(),
+//         title: "For Whom the Bell Tolls".to_string()
+//     };
+//     print_put(&mut dao, &book0);
+//
+//     let book_result0: Result<Option<Book>, DynamoDBError> = dao.get(&book0.book_id);
+//     print_result(&book0.book_id, book_result0);
+// }
--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,6 @@
 use uuid::Uuid;
 
-#[derive(Debug,PartialEq,Eq,Hash)]
+#[derive(Debug,PartialEq,Eq,Hash,RustcEncodable,RustcDecodable)]
 pub struct Book {
     pub book_id: Uuid,
     pub author: String,
